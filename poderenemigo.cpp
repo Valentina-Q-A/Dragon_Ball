05de0276aa175b904356a1cb49874dd7a31f83e5
Juan Felipe Orozco Londoño <juan.orozcol1@ieee.org>
2025-07-13 22:43:31 -0500

    Versión final con nivel 2 y excepciones

--- poderenemigo.cpp
+++ poderenemigo.cpp
@@ -25,13 +25,23 @@ void PoderEnemigo::mover() {
     moveBy(direccion.x() * velocidad, direccion.y() * velocidad);
 
     if (collidesWithItem(jugadorObjetivo)) {
+        // Aplicar empuje solo si está en el Nivel 2
+        if (jugadorObjetivo->estaEnNivel2) {
+            qreal direccion = (x() < jugadorObjetivo->pos().x()) ? 20.0 : -20.0;
+            jugadorObjetivo->moveBy(direccion, 0);
+        }
+
         jugadorObjetivo->recibirGolpe();
+
         escenaRef->removeItem(this);
         deleteLater();
+        return;
     }
 
+    // por si sale del área
     if (x() < 0 || x() > 800 || y() < 0 || y() > 600) {
         escenaRef->removeItem(this);
         deleteLater();
     }
 }
+
