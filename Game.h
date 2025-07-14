0c300c50a16eec2b472578fcd65d59ed1be58280
Valentina-Q-A <62760903+Valentina-Q-A@users.noreply.github.com>
2025-07-13 19:50:48 -0500

    Versión final con nivel dos completo

--- Game.h
+++ Game.h
@@ -5,25 +5,23 @@
 #include <QGraphicsScene>
 #include "../personajes/Jugador.h"
 #include "../niveles/nivel1.h"
+#include "../niveles/nivel2.h"  // ← NUEVO
 
 class Game : public QGraphicsView {
     Q_OBJECT
 public:
     Game(QWidget *parent = nullptr);
-    //ESTO NO ESTABA
     void start();
     void keyPressEvent(QKeyEvent* event) override;
-    //void keyReleaseEvent(QKeyEvent* event) override;
 
+public slots:
+    void cambiarANivel2();  // ← NUEVO
 
 private:
     QGraphicsScene *scene;
     Jugador *jugador;
-    //HASTA AQUI FUNCIONABA
     Nivel1* nivel1;
-    //QTimer* timerMovimiento;  // ← NUEVO
-    /*private slots:
-    void actualizarMovimiento();*/
+    Nivel2* nivel2 = nullptr;  // ← NUEVO
 };
 
 #endif // GAME_H
