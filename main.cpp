170a7c5f71b6d368354410cee3fbcd15ac28fefb
Juan Felipe Orozco Londoño <juan.orozcol1@ieee.org>
2025-07-13 22:41:40 -0500

    Versión final con nivel 2 y excepciones

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 #include <QApplication>
 #include "control/Game.h"
+#include <QMessageBox>
+#include <stdexcept>  // Para runtime_error
+
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    //QGraphicsView vista;
 
-    Game *game = new Game();
-    game->show();
-
-    //MIRAR SIN ESTO
-    game->start();
-    game->setFocus();  // ← Muy importante
+    try {
+        Game *game = new Game();
+        game->show();
+        game->start();
+    } catch (const std::runtime_error& e) {
+        QMessageBox::critical(nullptr, "Error crítico", e.what());
+        return -1;
+    }
 
     return a.exec();
 }
