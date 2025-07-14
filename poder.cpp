df95a9443d05c524e49ab7d84bc45ffd58406b74
Valentina-Q-A <62760903+Valentina-Q-A@users.noreply.github.com>
2025-07-13 20:46:19 -0500

    Versión final con nivel dos completo y excepción

--- poder.cpp
+++ poder.cpp
@@ -3,11 +3,21 @@
 #include "qgraphicsscene.h"
 #include <QtMath>
 #include <QList>
+#include "hombreenmascarado.h"
+#include "poderenemigo.h"
+#include "jugador.h"
+#include <stdexcept>
+
+
 
 Poder::Poder(QPointF inicio, QPointF destino, float velocidad, QGraphicsScene* escena)
     : velocidad(velocidad), escenaRef(escena) {
+    QPixmap sprite(":/images/poder.png");
+    if (sprite.isNull()) {
+        throw std::runtime_error("No se pudo cargar el sprite del poder");
+    }
+    setPixmap(sprite.scaled(40, 40));
 
-    setPixmap(QPixmap(":/images/poder.png").scaled(40, 40)); // Cargá tu sprite real
     setPos(inicio);
     escena->addItem(this);
 
@@ -38,9 +48,48 @@ void Poder::mover() {
             return;
         }
 
+        if (HombreEnmascarado* enemigo2 = dynamic_cast<HombreEnmascarado*>(item)) {
+
+            qreal direccion = (x() < enemigo2->pos().x()) ? 20.0 : -20.0;
+            enemigo2->moveBy(direccion, 0);
+            enemigo2->recibirDanio();
+
+            // Efecto visual de daño
+            enemigo2->setOpacity(0.6);
+            QTimer::singleShot(150, enemigo2, [enemigo2]() {
+                enemigo2->setOpacity(1.0);
+            });
+
+            escenaRef->removeItem(this);
+            deleteLater();
+            return;
+        }
+
+        // Empuje energético si choca con poder enemigo
+        if (PoderEnemigo* poderEnemigo = dynamic_cast<PoderEnemigo*>(item)) {
+
+            QList<QGraphicsItem*> items = escenaRef->items();
+            for (QGraphicsItem* obj : items) {
+                if (Jugador* goku = dynamic_cast<Jugador*>(obj)) {
+                    goku->moveBy(-20, 0);
+                }
+                if (HombreEnmascarado* enemigo = dynamic_cast<HombreEnmascarado*>(obj)) {
+                    enemigo->moveBy(20, 0);
+                }
+            }
+
+            // Destruir ambos poderes
+            escenaRef->removeItem(poderEnemigo);
+            poderEnemigo->deleteLater();
+
+            escenaRef->removeItem(this);
+            deleteLater();
+            return;
+        }
+
     }
 
-    // Opcional: eliminar si se sale del mapa
+    // eliminar si se sale del mapa
     if (x() < 0 || x() > 800 || y() < 0 || y() > 600) {
         escenaRef->removeItem(this);
         deleteLater();
